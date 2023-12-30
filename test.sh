04d60f852f17799a9fb0c8c04855bd8e17626511
Huan Bui <huuuhuannt1998@gmail.com>
2023-12-29 20:54:24 -0500

    update

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables
-set timeout -1
+
 channel_name="shadow-driver"
 channel_description="Shadow Driver"
 channel_terms_url="http://uncc.edu"
@@ -16,23 +16,10 @@ mkdir "$newfolder"
 
 cp -r "$foldername"/* "$newfolder"
 
-# Start the command
-spawn ./smartthings.exe edge:channels:create
+./smartthings.exe edge:drivers:package "$newfolder"
 
-# Expect the channel name prompt and send the channel name
-expect "? Channel name:"
-send "$channel_name\r"
+./smartthings.exe edge:channels:create
 
-# Expect the channel description prompt and send the description
-expect "? Channel description:"
-send "$channel_description\r"
-
-# Expect the terms of service URL prompt and send the URL
-expect "? Channel terms of service URL:"
-send "$channel_terms_url\r"
-
-# Wait for the command to complete
-expect eof
 
 
 
